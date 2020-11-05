48cd1ef9a8f0f925155c4932ce2b7468d42d6383
Abakayfl <73713860+Abakayfl@users.noreply.github.com>
2020-11-04 19:39:02 -0500

    Adding Pokemon Variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-alert('Hello world!');
-let favoriteFood = 'Rice, Fries and Salmon';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+	name: "Bulbasaur", 
+	height: 0.7, 
+	abilities: ["grass", "poison"]
+  }, 
+  {
+    name: "Pikachu", 
+	height: 0.4, 
+	abilities: ["static","lightningrod"]
+  },
+  {
+    name: "Squirtle", 
+    height: 0.5, 
+    abilities: ["rain-dish", "torrent"]
+  }
+];
