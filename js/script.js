84dd3fb8c163e980b360289190d76fef13e2220c
Abakayfl <73713860+Abakayfl@users.noreply.github.com>
2020-11-10 08:37:36 -0500

    Functions and IIFE (Draft Version)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-let pokemonList = [{
+let pokemonRepository = (function () {
+
+let pokemonList = [
+{
   name: "Bulbasaur", 
   height: 0.7, 
   abilities: ["grass", "poison"]
@@ -14,16 +17,45 @@ let pokemonList = [{
   abilities: ["rain-dish", "torrent"]
 }
 ];
-  //start from the first element from array list all of them
-for (let i=0; i < pokemonList.length; i++) {
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+}
+
+  function getAll() {
+    return pokemonList;
+}
+
+  return {
+    add: add,
+    getAll: getAll
+};
+})();
+
+
+  pokemonList.forEach(function(pokemon) {
   //Pick the pokemon name and height if height is greater than 0.6 then write this statement "-Wow, that's big"
-  if (pokemonList[i].height >0.6){
-    document.write(pokemonList[i].name + " (" + "height: " + pokemonList[i].height + ")" + " - Wow, that's big!" + "<br>"); 
+  if (pokemonList.height >0.6){
+    document.write(pokemonList.name + " (" + "height: " + pokemonList.height + ")" + " - Wow, that's big!" + "<br>"); 
   }
   //if the pokemon height is less than 0.6 then write this statement "is a pokemon"
   else{
-    document.write(pokemonList[i].name + " (" + "height: " + pokemonList[i].height + ")" + " is a pokemon " + "<br>"); 
+    document.write(pokemonList.name + " (" + "height: " + pokemonList.height + ")" + " is a pokemon " + "<br>"); 
   }
-}
+});
+
+
+//   for (let i=0; i < pokemonList.length; i++) {
+//   //Pick the pokemon name and height if height is greater than 0.6 then write this statement "-Wow, that's big"
+//   if (pokemonList[i].height >0.6){
+//     document.write(pokemonList[i].name + " (" + "height: " + pokemonList[i].height + ")" + " - Wow, that's big!" + "<br>"); 
+//   }
+//   //if the pokemon height is less than 0.6 then write this statement "is a pokemon"
+//   else{
+//     document.write(pokemonList[i].name + " (" + "height: " + pokemonList[i].height + ")" + " is a pokemon " + "<br>"); 
+//   }
+// }
+
+
 
 
