ced13395528801914d0ad62d44408773021b37f6
Abakayfl <73713860+Abakayfl@users.noreply.github.com>
2020-11-18 07:17:37 -0500

    Fixing js path

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ let pokemonRepository = (function () {
 
 pokemonRepository.getAll().forEach(function (pokemon) {
 
-  console.log(pokemon)
+  console.log(pokemon);
 
   if (pokemon.height > 0.6){
 
